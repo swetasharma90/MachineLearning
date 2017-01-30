709ca95e1f831a4a08a002251a8d24ed6642b00e
pearlorchids <sharma.sweta1990@gmail.com>
2017-01-30 21:25:06 +0530

    Add files via upload

---
+++ Code/svp.m
@@ -0,0 +1,29 @@
+function [U S V]=svp(omega,b,k,m,n)
+
+%get indices back for making matrix from vector of non-zero values
+[I, J] = ind2sub([m,n], omega);
+
+t = 1;
+X = zeros(m,n);
+
+t_norm = norm(X(omega) - b);
+
+epsilon = 0.0001;
+
+while(t_norm > epsilon)
+    k_svd=X(omega) - (1/t)*(X(omega) -b);
+    
+    %making matrix to perform svd
+    svd_X=zeros(m,n);
+    for i=1:size(b)
+        svd_X(I(i),J(i))=k_svd(i);
+    end
+    
+    [U,S,V] = svd(svd_X);
+    %k-rank approximation
+    X = U(1:m,1:k)*S(1:k,1:k)*V(1:n,1:k)';
+    
+    t = t+1;
+    t_norm = norm(X(omega) - b)
+end
+end
